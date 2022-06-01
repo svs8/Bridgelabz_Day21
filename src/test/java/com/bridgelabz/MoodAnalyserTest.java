558a176882248b9bc35718c14772041e39093a75
svs8 <surakshithvittalshetty@gmail.com>
2022-06-01 23:10:51 +0530

    Refactored the code

--- src/test/java/com/bridgelabz/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -1,12 +1,13 @@
 package com.bridgelabz;
 
 import org.junit.Assert;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MoodAnalyserTest {
 
-    @org.junit.jupiter.api.Test
+    @Test
     public void givenMessage_WhenSad_ShouldReturnSad() {
         MoodAnalyser moodanalyser = new MoodAnalyser("Iam in sad mood");
         String mood= moodanalyser.analyseMood();
@@ -15,7 +16,7 @@ class MoodAnalyserTest {
     }
 
 
-    @org.junit.jupiter.api.Test
+    @Test
     void givenMessage_WhenNotSad_ShouldReturnHappy() {
         MoodAnalyser moodanalyser=new MoodAnalyser("Iam in any mood");
         String mood=moodanalyser.analyseMood();
@@ -23,14 +24,14 @@ class MoodAnalyserTest {
     }
 
 
-    @org.junit.jupiter.api.Test
+    @Test
     public void givenNullMood_ShouldReturnHappy() {
         MoodAnalyser moodanalyser = new MoodAnalyser(null);
         String mood = moodanalyser.analyseMood();
         Assert.assertEquals("Happy", mood);
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     public void givenNullMood_ShouldReturnCustomException() {
         MoodAnalyser moodanalyser = new MoodAnalyser(null);
         String mood= null;
